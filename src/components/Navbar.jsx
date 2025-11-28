0eeb0a011a8cdddd2fc65822e31f640753d8b222
Naim Paz <naimpaz274@gmail.com>
2025-11-27 22:51:53 -0300

    le agregue una animacion al boton

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,8 +31,29 @@ function Navbar() {
         </div>
 
         {/* Botón de reserva */}
-        <button className="reserve-button">Reservar Ahora</button>
+        <button className="reserve-button blob-btn">
+          Reservar Ahora
+          <span className="blob-btn__inner">
+            <span className="blob-btn__blobs">
+              <span className="blob-btn__blob"></span>
+              <span className="blob-btn__blob"></span>
+              <span className="blob-btn__blob"></span>
+              <span className="blob-btn__blob"></span>
+            </span>
+          </span>
+        </button>
       </div>
+      
+      {/* SVG Filter para el efecto gooey */}
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style={{ position: 'absolute', width: 0, height: 0 }}>
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
+            <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
+          </filter>
+        </defs>
+      </svg>
     </nav>
   )
 }
