28a7008e7c56a8093d9e7eb56c0c199e64b5f10c
Naim Paz <naimpaz274@gmail.com>
2025-12-02 23:13:48 -0300

    feat: crear estructura básica del Footer con estilos del navbar

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        {/* Contenido del footer */}
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
+
