58939a6d3748f58a7d09057e8df29021d6e39c75
Maxi Ordoñez <exemaxi@outlook.com>
2025-12-13 11:24:31 -0300

    Se agregaron las validaciones al ligin con la dependencia de react hook from.

--- src/components/ui/ModalLogin.jsx
+++ src/components/ui/ModalLogin.jsx
@@ -1,12 +1,27 @@
 import { Button, Modal, Form } from "react-bootstrap";
 import "./Modales.css";
+import { useForm } from "react-hook-form";
 
 export const ModalLogin = ({ showLogin, loginClose }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmi = (data) => {
+    console.log(data);
+    //Agregar logica de login.
+    reset();
+  };
+
+
   return (
     <>
       <Modal show={showLogin} onHide={loginClose}>
         <Modal.Body>
-          <Form className=" css-modal-login">
+          <Form className=" css-modal-login" onSubmit={handleSubmit(onSubmi)}>
             <div className="text-center mb-2">
               <h1 className="mb-2">Bienvenido</h1>
               <div>
@@ -17,7 +32,20 @@ export const ModalLogin = ({ showLogin, loginClose }) => {
             </div>
             <Form.Group className="mb-3">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="ejemplo@Email.com" />
+              <Form.Control
+                type="email"
+                placeholder="ejemplo@Email.com"
+                {...register("email", {
+                  required: "El email es requerido",
+                  pattern: {
+                    value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: "El email no es válido",
+                  },
+                })}
+              />
+              {errors.email && (
+                <span className="text-danger">{errors.email.message}</span>
+              )}
             </Form.Group>
 
             <Form.Group className="mb-3">
@@ -25,7 +53,18 @@ export const ModalLogin = ({ showLogin, loginClose }) => {
               <Form.Control
                 type="password"
                 placeholder="Ingresa tu contraseña"
+                {...register("password", {
+                  required: "El password es requerido",
+                  pattern: {
+                    value:
+                      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#.])[A-Za-z\d@$!%*?&#.]{8,}$/,
+                    message: "El password no es válido",
+                  },
+                })}
               />
+              {errors.password && (
+                <span className="text-danger">{errors.password.message}</span>
+              )}
             </Form.Group>
 
             <Button variant="primary" type="submit" className="w-100">
